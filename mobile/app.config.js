69c78a7f01c249c4805cc7de11fc6e4fa147c8d8
Jason Galuten <jason@galuten.com>
2025-11-25 12:54:24 -0800

    Update API URL in configuration files and modify postinstall script
    - Change default EXPO_PUBLIC_API_URL in app.config.js and eas.json to the new production API endpoint
    - Update postinstall script in package.json to use npx for patch-package with fallback

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,10 +1,9 @@
-const baseConfig = require('./app.json');
+const baseConfig = require("./app.json");
 
 module.exports = ({ config }) => {
   // Set default API URL for production if not provided via environment
   if (!process.env.EXPO_PUBLIC_API_URL) {
-    // TODO: Update this to your production API URL when deployed
-    process.env.EXPO_PUBLIC_API_URL = 'https://api.tapstory.app';
+    process.env.EXPO_PUBLIC_API_URL = "https://tap-story-api.onrender.com";
   }
 
   return {
